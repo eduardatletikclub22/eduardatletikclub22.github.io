ee79a4baf6cd9edc03b35c8fdd06de17e72e05af
eduardatletikclub22 <98297564+eduardatletikclub22@users.noreply.github.com>
2022-01-24 05:18:58 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+    <title>Eduard Atletik Club</title>
+</head>
+
+<body class="mt-5">
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="">Eduard Atletik Club</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Pelatih</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="">Tentang Kami</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img src="eac.png" width="25%" class="rounded-circle "><br>
+            <img src="eac1.png" width="150">
+            <h1 class="display-4">Eduard Atletik Club</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+        </div>
+    </div>
+
+
+    <div class="container">
+        <div class="row mb-4">
+            <div class="col text-center">
+                <h2>About</h2>
+            </div>
+        </div>
+
+        <div class="row justify-content-center">
+            <div class="col-md-5 text-center">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro cupiditate excepturi error possimus consequuntur quos similique alias? Repellat porro consectetur quaerat nostrum, reprehenderit ad voluptatibus laborum,.</p>
+            </div>
+            <div class="col-md-5 text-center">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero dicta molestias placeat amet cum mollitia in delectus, voluptates nihil, illum nam cupiditate provident adipisci. Error voluptates illum esse dicta non.</p>
+            </div>
+        </div>
+
+    </div>
+
+
+    <section id="portfolio" class="portfolio bg-warning pb-4">
+        <div class="container">
+            <div class="row mb-4 pt-4">
+                <div class="col text-center">
+                    <h2>Porortfolio</h2>
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <div class="col-md">
+                    <div class="card mb-4">
+                        <img class="card-img-top" src="eac2.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md">
+                    <div class="card mb-4">
+                        <img class="card-img-top" src="eac2.jpg" alt="Card image cap">
+                        <div class="card-body">
+
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md">
+                    <div class="card mb-4">
+                        <img class="card-img-top" src="eac2.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mb-4">
+                <div class="col-md">
+                    <div class="card mb-4">
+                        <img class="card-img-top" src="eac2.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md">
+                    <div class="card mb-4">
+                        <img class="card-img-top" src="eac2.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md">
+                    <div class="card mb-4">
+                        <img class="card-img-top" src="eac2.jpg" alt="Card image cap">
+                        <div class="card-body">
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section id="contact" class="contact">
+        <div class="container">
+            <div class="row pt-4 mb-4">
+                <div class="col text-center">
+                    <h2>Contact Us</h2>
+                </div>
+            </div>
+
+            <div class="row justify-content-center">
+                <div class="col-lg-4">
+                    <div class="card bg-primary text-white text-center p-3 mb-4">
+                        <blockquote class="blockquote mb-0">
+                            <h5 class="card-title">Contact Us</h5>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat.</p>
+
+                        </blockquote>
+                    </div>
+
+                    <ul class="list-group mb-5">
+                        <li class="list-group-item active">
+                            <h1>Location</h1>
+                        </li>
+                        <li class="list-group-item">My office</li>
+                        <li class="list-group-item">Jln. SMA 48, No 28, Jakarta Timur</li>
+                        <li class="list-group-item">West Java, Indonesia</li>
+
+                    </ul>
+                </div>
+
+                <div class="col-lg-6">
+                    <form>
+                        <div class="form-group">
+                            <label for="Nama">Nama</label>
+                            <input type="text" class="form-control" id="Nama">
+                        </div>
+                        <div class="form-group">
+                            <label for="Email">Emaila</label>
+                            <input type="text" class="form-control" id="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for="telp">No Telp</label>
+                            <input type="text" class="form-control" id="telp">
+                        </div>
+                        <div class="form-group">
+                            <label for="pesan">Pesan</label>
+                            <textarea name="pesan" id="pesan" class="form-control"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <button type="button" class="btn btn-primary">Kirim Pesan</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <footer class="bg-dark text-white">
+        <div class="row">
+            <div class="col text-center pt-3">
+                <p>Eduard Atletik Club 2022</p>
+            </div>
+        </div>
+    </footer>
+
+
+
+</body>
+
+</html>
